4723b7aa271441ef759c30f1982e16671cc8bd15
Raniel Mendes <raniel.mendes@hotmail.com>
2021-10-04 08:58:41 -0300

    [Feature Rani - Task: Implemented TemaRepository]

--- src/main/java/com/Generation/Farmacia/Repository/CategoriaRepository.java
+++ src/main/java/com/Generation/Farmacia/Repository/CategoriaRepository.java
@@ -1,12 +1,15 @@
-package com.Generation.Farmacia.Repository;
-
-import java.util.List;
+package com.Generation.Farmacia.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Generation.Farmacia.model.CategoriaModel;
 
-public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long>{
-	public List<CategoriaModel> findAllByDescriçãoContainerIgnoreCase(String descricao);
+import java.util.List;
 
-} 
+@Repository
+public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long>{
+	
+	public List<CategoriaModel> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
