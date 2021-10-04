be16609532992635855c91b976b4f42875ec829b
Matheus <matheus.malmeida7@gmail.com>
2021-10-03 22:41:54 -0300

    [Feature Matheus - Task: Implemented Repository]

--- src/main/java/com/Generation/Farmacia/controller/ProdutoController.java
+++ src/main/java/com/Generation/Farmacia/controller/ProdutoController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.Generation.Farmacia.model.ProdutoModel;
 /*importa classe ProdutoRepository*/
+import com.Generation.Farmacia.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/produto")
---
+++ src/main/java/com/Generation/Farmacia/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.Generation.Farmacia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Generation.Farmacia.model.CategoriaModel;
+
+import java.util.List;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long>{
+	
+	public List<CategoriaModel> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
